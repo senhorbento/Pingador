98f65719fa7004c50d10ed8309773144846e50dc
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2021-06-07 19:39:44 -0300

    Update Sir_I.sh

--- Sir_I.sh
+++ Sir_I.sh
@@ -94,7 +94,7 @@ do
             else 
                 echo -n "$line - " >> valido.txt
             fi
-            ping -c 1 $line | egrep -wo "([0-9]?[0-9]?[0-9]% packet loss)" >> valido.txt
+            ping -s 24 -t 10 -c 1 $line | egrep -wo "([0-9]?[0-9]?[0-9]% packet loss)" >> valido.txt
             (( contv++ ))
             echo -en "*"
 
